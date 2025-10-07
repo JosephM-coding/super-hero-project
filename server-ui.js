e52f498b33956f61b7d20f2d2a223439fb8f6727
Rasmidah <rasmidahgh@gmail.com>
2025-10-07 14:27:19 -0700

    Add files via upload

--- server-ui.js
+++ server-ui.js
@@ -2,17 +2,19 @@ const express = require("express");
 const app = express();
 const fs = require("fs").promises;
 const path = require("path");
-const heroUIDBpath = path.join(__dirname, "data", "ui-heroes.json");
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.set("view engine", "ejs");
+
+const heroUIDBpath = path.join(__dirname, "data", "ui-heros.json");
 const PORT = 4000;
 
 // read, write functions
 const readUsers = () => {
     try {
-      return JSON.parse(fs.readFileSync(userDBfilePath, "utf8"))
+      return JSON.parse(fs.readFileSync(heroUIDBpath, "utf8"));
     } catch (e) {
       console.log('no users file found, initializing empty array');
       return [];
@@ -20,14 +22,18 @@ const readUsers = () => {
 }
 
 const writeUsers = (users) => {
-    const dataDir = path.dirname(userDBfilePath);
+    const dataDir = path.dirname(heroUIDBpath);
     if (!fs.existsSync(dataDir)) {
       fs.mkdirSync(dataDir, { recursive: true })
     }
     //write file code
-    fs.writeFileSync(userDBfilePath, JSON.stringify(users, null, 2));
+    fs.writeFileSync(heroUIDBpath, JSON.stringify(heroes, null, 2));
   }
 
+  app.get("/heros/list", (req, res) => {
+    const heroes = readUsers();
+    res.render("heroList", { heroes });
+  });
 // --- routes ----
 // ---- GET ----
 app.get("/", async (req, res) => {
